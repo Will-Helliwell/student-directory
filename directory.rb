2ddc29e06d779d14d3f0e83797d9d50ca1ab9cbc
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2020-11-17 08:40:15 +0000

    remove repetition from input_students method, accept input for cohort

--- directory.rb
+++ directory.rb
@@ -1,32 +1,52 @@
+
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(50)
 end
 
 def print(students)
   students.each { |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(50)
   }
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(50)
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
   students = Array.new
-  name = gets.chomp
-  while !name.empty?
-    students << {name: name, cohort: :november}
+  puts "Please enter the name and cohort of each student"
+  puts "To finish, just hit return twice"
+  
+  while true do
+    default_cohort = "november"
+    # get name, and break if nothing entered
+    puts "Name:"
+    name = gets.chomp
+    break if name == ""
+    # get cohort, only accepting months + nothing entered (which defaults to default_month)
+    while true do
+      cohorts = %w(january february march april may june july august september october november december)
+      puts "Cohort:"
+      cohort = gets.chomp.downcase
+      puts "captured: before#{cohort}after"
+      puts cohort.class
+      break if cohorts.include?(cohort)
+      if cohort == ""
+        cohort = default_cohort
+        break
+      end
+    end
+    # add name and cohort to array of hashes, print student count
+    students << { name: name, cohort: cohort.to_sym}
     puts "Now we have #{students.count} students"
-  name = gets.chomp
   end
+
   students
 end
 
 students = input_students
 print_header
 print(students)
-print_footer(students)
+#print_footer(students)
