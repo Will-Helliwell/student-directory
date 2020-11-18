5b66b086fe027cdf38fa00c04c0d197935cb859d
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2020-11-18 17:40:04 +0000

    change print function name to print_directory

--- directory.rb
+++ directory.rb
@@ -42,7 +42,7 @@ def print_menu
   puts "9. Exit"
 end
 
-def show_students
+def print_directory
   print_header
   print_students_list
   print_footer
@@ -51,7 +51,7 @@ end
 def process(selection)
   case selection
     when "1" then input_students
-    when "2" then show_students
+    when "2" then print_directory
     when "3" then save_students
     when "4" then load_students
     when "9" then exit
